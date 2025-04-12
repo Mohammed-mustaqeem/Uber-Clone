e8c884233ff1361afc3bf4f61c810fcb3cd87796
Mohammed Mustaqeem <mustaqeemcode@gmail.com>
2025-04-12 21:29:54 +0530

    frontend start

---
+++ Frontend/src/assets/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from '../../../public/images/logo.png'
+import bgImage from "../../../public/images/bg-2.avif";
+
+const Home = () => {
+  return (
+    <div>
+      <div
+        style={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className=" h-screen pt-5  w-full flex justify-between flex-col bg-red-500"
+      >
+        <img className="w-16 ml-5" src={logo} alt="" />
+        <div className="bg-white pb-7 py-4 px-4">
+          <h2 className="text-3xl font-bold">Get Started with Uber</h2>
+          <button className="w-full bg-black text-white py-3 rounded mt-5">
+            Continue
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home
---
+++ Frontend/src/assets/pages/UserLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserLogin = () => {
+  return (
+    <div>
+      userLogin
+    </div>
+  )
+}
+
+export default UserLogin
